24be63c83fa819df8d2256743a9b1670774b829f
ustulation <ustulation@gmail.com>
2018-10-08 14:20:29 +0100

    initial commit TcpSock impl Licences README and misc chore file

---
+++ src/error.rs
@@ -0,0 +1,44 @@
+use maidsafe_utilities::serialisation::SerialisationError;
+use mio::timer::TimerError;
+use std::io;
+
+quick_error! {
+    /// Common module specific error
+    #[derive(Debug)]
+    pub enum SocketError {
+        /// IO error
+        Io(e: io::Error) {
+            description(e.description())
+            display("Io error: {}", e)
+            cause(e)
+            from()
+        }
+        /// Socket is uninitialised and invalid for any operation
+        UninitialisedSocket {
+            description("Socket is uninitialised and invalid for any operation")
+            display("Socket is uninitialised and invalid for any operation")
+        }
+        /// Size of a message to send or about to be read is too large
+        PayloadSizeProhibitive {
+            description("Payload is too large")
+        }
+        /// Serialisation error
+        Serialisation(e: SerialisationError) {
+            description(e.description())
+            display("Serialisation error: {}", e)
+            cause(e)
+            from()
+        }
+        /// Timer error
+        Timer(e: TimerError) {
+            description(e.description())
+            display("Timer error: {}", e)
+            cause(e)
+            from()
+        }
+        /// A zero byte socket read - means EOF
+        ZeroByteRead {
+            description("Read zero bytes from the socket - indicates EOF")
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,43 @@
+//! # Overview
+//!
+//! The goal of this crate is to provide a collection of async sockets which can be used out of the
+//! box with `mio` event loop. As a simple example, using stream based protocols will require some
+//! sort of mechanism to determine the boundaries of a message etc., and this crate provides default
+//! implementation to handle those and abstract the boilerplate from the user libs.
+
+#[macro_use]
+extern crate log;
+#[cfg_attr(feature = "cargo-clippy", allow(useless_attribute))]
+#[macro_use]
+extern crate quick_error;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate unwrap;
+
+extern crate byteorder;
+extern crate maidsafe_utilities;
+extern crate mio;
+extern crate net2;
+extern crate serde;
+
+pub use error::SocketError;
+pub use tcp_sock::TcpSock;
+
+mod error;
+mod tcp_sock;
+mod udt_sock;
+
+/// Priority of a message to be sent by Crust. A lower value means a higher priority, so Priority 0
+/// is the highest one. Low-priority messages will be preempted if need be to allow higher priority
+/// messages through. Messages with a value `>= MSG_DROP_PRIORITY` will even be dropped, if
+/// bandwidth is insufficient.
+pub type Priority = u8;
+
+pub const MAX_PAYLOAD_SIZE: usize = 2 * 1024 * 1024;
+/// Minimum priority for droppable messages. Messages with lower values will never be dropped.
+pub const MSG_DROP_PRIORITY: u8 = 2;
+/// Maximum age of a message waiting to be sent. If a message is older, the queue is dropped.
+pub const MAX_MSG_AGE_SECS: u64 = 60;
+/// `Result` type specialised for this crate
+pub type Res<T> = Result<T, SocketError>;
